5baa37a37d904f8419e15540c274885e25c10236
Prithviraj Dasgupta <mimo_1225@Prithvirajs-MacBook-Pro-2.local>
2025-06-26 01:38:16 +0530

    Initial commit for Mimo.ai 🚀

---
+++ analyze_track.py
@@ -0,0 +1,14 @@
+import essentia
+import essentia.standard as es
+
+loader = es.MonoLoader(filename='testbeat.mp3')  # or 'yourfile.wav'
+audio = loader()
+
+rhythm_extractor = es.RhythmExtractor2013(method="multifeature")
+bpm, _, _, _, _ = rhythm_extractor(audio)
+
+key_extractor = es.KeyExtractor()
+key, scale, strength = key_extractor(audio)
+
+print(f"BPM: {bpm}")
+print(f"Key: {key} {scale} (Strength: {strength})")
