65d726ad203242bf8765724ed6ee30555924ca69
Mimo <prithviraj.mimo@gmail.com>
2025-06-29 01:32:16 +0530

    Initial Commit

---
+++ MimoAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MimoAI
+//
+//  Created by Prithviraj Dasgupta on 29/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MimoAI/MimoAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MimoAIApp.swift
+//  MimoAI
+//
+//  Created by Prithviraj Dasgupta on 29/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MimoAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
